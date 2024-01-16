70cfb56f120b55ebde9db61976ea1cde677050b5
Atharva Mulgund <atharva.mulgund@gmail.com>
2024-01-17 01:07:20 +0530

    Added new diet

--- src/pages/Diet.react.jsx
+++ src/pages/Diet.react.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState,useEffect } from "react";
 import {
   Box,
   Container,
@@ -7,6 +7,8 @@ import {
   CardActions,
   CardContent,
   CardMedia,
+  Chip,
+  Button,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { homePageDiet } from "../data/data";
@@ -14,8 +16,32 @@ import { homePageDiet } from "../data/data";
 import HomeHeader from "../components/header/HomeHeader.react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
+import { ArrowBack, ArrowForward } from "@mui/icons-material";
 
 const Diet = () => {
+  const [foodDiet, setFoodDiet] = useState([])
+  const url = 'https://edamam-recipe-search.p.rapidapi.com/api/recipes/v2?type=public&co2EmissionsClass=A%2B&field%5B0%5D=uri&beta=true&random=true&cuisineType%5B0%5D=American&imageSize%5B0%5D=LARGE&mealType%5B0%5D=Breakfast&health%5B0%5D=alcohol-cocktail&diet%5B0%5D=balanced&dishType%5B0%5D=Biscuits%20and%20cookies';
+const options = {
+	method: 'GET',
+	headers: {
+		'Accept-Language': 'en',
+		'X-RapidAPI-Key': process.env.REACT_APP_RECIEPE_API_KEY,
+		'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
+	}
+};
+
+  useEffect(() => {
+ const fetchData = async () => {
+  try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	setFoodDiet(result)
+} catch (error) {
+	console.error(error);
+}
+}
+fetchData()
+  }, [])
   const navigate = useNavigate();
 
   if (!Cookies.get("userID")) {
@@ -30,7 +56,7 @@ const Diet = () => {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          height: { lg: "100vh", sm: "100%", xs: "100%" },
+          height: { lg: "100%", sm: "100%", xs: "100%" },
           textAlign: "center",
           flexDirection: "column",
         }}
@@ -59,7 +85,7 @@ const Diet = () => {
           }}
           id="diet"
         >
-          {homePageDiet.map((item, index) => {
+          {foodDiet?.hits?.map((item, index) => {
             return (
               <Link to={item.route} className="link-primary">
                 <CardActions>
@@ -70,12 +96,26 @@ const Diet = () => {
                     <CardMedia
                       component="img"
                       alt="green iguana"
-                      image={item.image}
+                      maxWidth='420px'
+                      image={item?.recipe?.image}
                     />
                     <CardContent>
                       <Typography gutterBottom variant="h5" component="div">
-                        {item.title}
+                        {item?.recipe?.label}
                       </Typography>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          gap: "0.8rem",
+                          flexWrap: "wrap",
+                        }}
+                      >
+                        {item?.recipe?.healthLabels?.slice(0, 5)?.map((hl) => (
+                          <Chip key={hl} label={hl} />
+                        ))}
+                      </Box>
                       <Typography
                         variant="body2"
                         color="primary"
@@ -86,6 +126,9 @@ const Diet = () => {
                       >
                         {item.description}
                       </Typography>
+                      <CardActions>
+                        <Button size="small">Explore <ArrowForward/></Button>
+                      </CardActions>
                     </CardContent>
                   </Card>
                 </CardActions>
